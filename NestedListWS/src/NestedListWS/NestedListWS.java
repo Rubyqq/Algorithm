8ffa29bc0e4de7386ebacf858e813bf06955bc02
Rubyqq <qiumy0321@gmail.com>
2017-03-13 01:06:54 -0400

    This is the solution of Nested List Weighted Sum from leetcode

---
+++ NestedListWS/src/NestedListWS/NestedListWS.java
@@ -0,0 +1,57 @@
+package NestedListWS;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class NestedListWS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	  // This is the interface that allows for creating nested lists.
+	  // You should not implement it, or speculate about its implementation
+	  public interface NestedInteger {
+	 
+	      // @return true if this NestedInteger holds a single integer, rather than a nested list.
+	      public boolean isInteger();
+	 
+	      // @return the single integer that this NestedInteger holds, if it holds a single integer
+	      // Return null if this NestedInteger holds a nested list
+	      public Integer getInteger();
+	 
+	      // @return the nested list that this NestedInteger holds, if it holds a nested list
+	      // Return null if this NestedInteger holds a single integer
+	      public LinkedList<NestedInteger> getList();
+	  }
+	 
+	
+	  public int depthSum(LinkedList<NestedInteger> nestedList) {
+	        
+	        int level = 1; 
+	        int total = 0;
+	        
+	        while(nestedList.size() != 0){
+	        
+	            LinkedList<NestedInteger> newList = new LinkedList<NestedInteger>();
+	            
+	            for(NestedInteger ni : nestedList){
+	                if(ni.isInteger()){
+	                    total += ni.getInteger() * level;
+	                }else{
+	                    newList.addAll(ni.getList()); 
+	                }
+	            }
+	            
+	            nestedList = newList;
+	            level++;
+	        }
+	        
+	        return total;
+	    }
+
+}
+	
+
