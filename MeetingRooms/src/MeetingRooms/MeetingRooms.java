4fdcd8eb0f7887ff384889791ed8ee439c7ff56d
Rubyqq <qiumy0321@gmail.com>
2017-03-18 00:30:30 -0400

    This is the solution of the question of Meeting Rooms from leetcode. The author use merge sort to solve the question and it is very quick although the code is a little bit long

--- MeetingRooms/src/MeetingRooms/MeetingRooms.java
+++ MeetingRooms/src/MeetingRooms/MeetingRooms.java
@@ -2,6 +2,16 @@ package MeetingRooms;
 
 import java.util.Arrays;
 
+/**
+ * Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), 
+ * determine if a person could attend all meetings.
+ * For example,
+ * Given [[0, 30],[5, 10],[15, 20]],
+ * return false. 
+ * @author Mengyi
+ *
+ */
+
 public class MeetingRooms {
 
 	public static void main(String[] args) {
