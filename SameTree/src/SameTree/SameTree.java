1775e3e78943a667c97a6c096a775cd91e6bb6a7
Rubyqq <qiumy0321@gmail.com>
2017-03-20 23:40:00 -0400

    This is the solution of question Same Tree from leetcode

--- SameTree/src/SameTree/SameTree.java
+++ SameTree/src/SameTree/SameTree.java
@@ -4,7 +4,16 @@ public class SameTree {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//stub
 	}
+	
+	public static boolean isSameTree(TreeNode p, TreeNode q) {
+        
+        if(p == null && q == null) return true;
+        if((p == null && q != null) || (p != null && q == null) ) return false;
+        
+        if(p.val == q.val) return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        else{return false;}
+    }
 
 }
