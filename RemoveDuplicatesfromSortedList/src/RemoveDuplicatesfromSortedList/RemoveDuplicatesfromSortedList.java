7e86a7e0696f622506b3943688a0167bb75450bd
Rubyqq <qiumy0321@gmail.com>
2017-03-27 05:39:26 -0400

    This is the solution of question Remove Duplicates from Sorted List from leetcode

--- RemoveDuplicatesfromSortedList/src/RemoveDuplicatesfromSortedList/RemoveDuplicatesfromSortedList.java
+++ RemoveDuplicatesfromSortedList/src/RemoveDuplicatesfromSortedList/RemoveDuplicatesfromSortedList.java
@@ -1,5 +1,17 @@
 package RemoveDuplicatesfromSortedList;
 
+/**
+ * Description of the question
+ * 
+ * Given a sorted linked list, delete all duplicates such that each element appear only once. 
+ * For example,
+ * Given 1->1->2, return 1->2.
+ * Given 1->1->2->3->3, return 1->2->3. 
+ * 
+ * @author Mengyi
+ *
+ */
+
 public class RemoveDuplicatesfromSortedList {
 
 	public static void main(String[] args) {
